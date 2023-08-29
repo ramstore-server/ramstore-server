4d3a825c6223adf5d5b8b9b12f60757901e9e146
Steve McKay <steve@b.abbies.us>
2023-08-29 11:05:56 -0500

    read a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use rustix_uring::{opcode, types, IoUring};
+use std::os::unix::io::AsRawFd;
+use std::{fs, io};
+
+fn main() -> io::Result<()> {
+    let mut ring = IoUring::new(8)?;
+
+    let fd = fs::File::open("LICENSE")?;
+    let mut buf = vec![0; 1024];
+
+    let read_e = opcode::Read::new(types::Fd(fd.as_raw_fd()), buf.as_mut_ptr(), buf.len() as _)
+        .build()
+        .user_data(types::IoringUserData { u64_: 0x42 });
+
+    unsafe {
+        ring.submission()
+            .push(&read_e)
+            .expect("submission queue is full");
+    }
+
+    ring.submit_and_wait(1)?;
+
+    let cqe = ring.completion().next().expect("completion queue is empty");
+
+    assert_eq!(cqe.user_data().u64_(), 0x42);
+    assert!(cqe.result() >= 0, "read error: {}", cqe.result());
+
+    println!("{}", String::from_utf8(buf).unwrap());
+    Ok(())
 }
